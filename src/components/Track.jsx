5a3e4c25977bd86179c73866849a6ab32c309dd8
shakBensa <shaked1bs@gmail.com>
2021-12-20 23:27:31 +0200

    1st commit

---
+++ src/components/Track.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Track(props) {
+    return (
+        <div >
+            <img className='cover' style={{boxShadow:'2px 3px 15px'}} width='200px' height='200px' src={props.img} alt={props.name}  />
+            <link rel="stylesheet" href="" />
+            <div className='platforms'>
+            <a href={props.spotify.link} target="_blank" rel="noreferrer" > <img width='45px' height='45px' src={props.spotify.img} alt={props.name} /></a>
+            <a href={props.apple.link} target="_blank" rel="noreferrer"> <img  width='45px' height='45px'src={props.apple.img} alt={props.name} /></a>
+           <a href={props.deezer.link} target="_blank" rel="noreferrer"> <img width='45px' height='45px' src={props.deezer.img} alt={props.name} /></a>
+            {/* <a href={props.tidal.link}> <img src={props.tidal.img} alt={props.name} /></a>  */}
+            </div>
+        </div>
+    )
+}
