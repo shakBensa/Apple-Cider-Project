d49e139941a21a7c7e2b2bc80273e5524e29de50
shakBensa <shaked1bs@gmail.com>
2021-12-26 00:11:17 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,23 @@ export default function About() {
     return (
         <div className='about' style={{ height: '100vh',width:'auto' }}>
             <br />
-<img className='logo' src='logo.jpg' alt='logo' />
+<img style={{marginBottom:'20px'}} className='logo' src='logo.jpg' alt='logo' />
+
 
             <p className='about' >Hey,my name is Shaked and I've been producing music for over a decade. <br />
                 In 2020 I've released my first track and even though I had years of exprience I was worrying about what kind of feedback I will recieve. <br />
                 Fortunatly,this release got endoresed by amazing ChillHop & Lofi Curators such as 
-                <a className='endorsements' href="https://www.instagram.com/chillselect"> Chill Select</a>,
-                <a className='endorsements' href="https://www.thebeatforest.com/"> The Beatforest</a>,
-                <a className='endorsements' href="https://www.chilledcatmusic.com/"> ChilledCat</a>,
-                <a className='endorsements' href="https://www.thewordisbond.com/"> The Word Is Bond </a>
+                <a className='endorsements' href="https://www.instagram.com/chillselect" target="_blank" rel="noreferrer"> Chill Select</a>,
+                <a className='endorsements' href="https://www.thebeatforest.com/" target="_blank" rel="noreferrer"> The Beatforest</a>,
+                <a className='endorsements' href="https://www.chilledcatmusic.com/" target="_blank" rel="noreferrer"> ChilledCat</a>,
+                <a className='endorsements' href="https://www.thewordisbond.com/" target="_blank" rel="noreferrer"> The Word Is Bond </a>
                 and many more awesome people that helped my music get heard along the way. <br />
                 <br />Since then I've amassed over 400K streams on Spotify. <br />There is no "End Goal" for me at the moment. <br />
                 The feeling I get when I make beats is unmatched,<br />
                 I do it for the love and I don't see myself stopping anytime soon.
                 <br />
                 <br />
-                if you'd like to work with me shoot me an email: shakedbravo@gmail.com
+                I Produce,Mix & Master my music so if you like what you hear and you want to work shoot me an email: shakedbravo@gmail.com
 
 
             </p>
